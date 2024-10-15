e2e8a4e79b1f2856225808e1fa77c35d9748bd31
ANDERSON R OLIVEIRA <103950004+eecaetano@users.noreply.github.com>
2024-10-15 00:23:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const response = await fetch('/api/dados');
-  const data = await response.json();
-  const dadosDiv = document.getElementById('dados');
-  dadosDiv.innerHTML = `
-    <p>Data: ${new Date().toLocaleDateString()}</p>
-    <p>Nível: ${data.Nivel}%</p>
-    <p>Volume: ${data.Volume} hm³</p>
-  `;
+  try {
+    const response = await fetch('/api/dados');
+    if (!response.ok) {
+      throw new Error('Erro ao buscar os dados');
+    }
+    const data = await response.json();
+    const dadosDiv = document.getElementById('dados');
+    dadosDiv.innerHTML = `
+      <p>Data: ${new Date().toLocaleDateString()}</p>
+      <p>Nível: ${data.Nivel}%</p>
+      <p>Volume: ${data.Volume} hm³</p>
+    `;
+  } catch (error) {
+    console.error('Erro:', error);
+    const dadosDiv = document.getElementById('dados');
+    dadosDiv.innerHTML = '<p>Erro ao carregar os dados.</p>';
+  }
 });
